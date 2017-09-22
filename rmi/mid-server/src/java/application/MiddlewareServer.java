e6b4b46d03c199e18ec8fd08548cdee25121d9ac
Amir Bawab <amirbawab@gmail.com>
2017-09-22 16:47:48 -0400

    Redirect clients calls to corresponding RM

--- rmi/mid-server/src/java/application/MiddlewareServer.java
+++ rmi/mid-server/src/java/application/MiddlewareServer.java
@@ -10,6 +10,10 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.Vector;
 
 class MiddlewareServer implements ResourceManager {
+    private ResourceManager m_carRM;
+    private ResourceManager m_flightRM;
+    private ResourceManager m_roomRM;
+
     public static void main(String[] args) {
 
         // Figure out where server is running
@@ -25,10 +29,10 @@ class MiddlewareServer implements ResourceManager {
         int rmRMIRegistryPort = Integer.parseInt(args[2]);
 
         // Connect
-        bindRM(ResourceManager.MID_SERVER_REF, serverRMIRegistryPort);
-        ResourceManager carRM = connectToRM(ResourceManager.RM_CAR_REF, rmRMIRegistryIP, rmRMIRegistryPort);
-        ResourceManager flightRM = connectToRM(ResourceManager.RM_FLIGHT_REF, rmRMIRegistryIP, rmRMIRegistryPort);
-        ResourceManager roomRM = connectToRM(ResourceManager.RM_ROOM_REF, rmRMIRegistryIP, rmRMIRegistryPort);
+        MiddlewareServer ms = bindRM(ResourceManager.MID_SERVER_REF, serverRMIRegistryPort);
+        ms.m_carRM = connectToRM(ResourceManager.RM_CAR_REF, rmRMIRegistryIP, rmRMIRegistryPort);
+        ms.m_flightRM = connectToRM(ResourceManager.RM_FLIGHT_REF, rmRMIRegistryIP, rmRMIRegistryPort);
+        ms.m_roomRM = connectToRM(ResourceManager.RM_ROOM_REF, rmRMIRegistryIP, rmRMIRegistryPort);
 
         // Create and install a security manager
         if (System.getSecurityManager() == null) {
@@ -37,7 +41,7 @@ class MiddlewareServer implements ResourceManager {
 
     }
 
-    private static void bindRM(String key, int port) {
+    private static MiddlewareServer bindRM(String key, int port) {
         // Bind server object
         try {
             // create a new Server object
@@ -47,11 +51,11 @@ class MiddlewareServer implements ResourceManager {
 
             // Bind the remote object's stub in the registry
             Registry registry = LocateRegistry.getRegistry(port);
-            //TODO Store in remote project
             registry.rebind(key, rm);
-            System.err.println("Server ready");
+            System.out.println("Middleware server ready!");
+            return obj;
         } catch (Exception e) {
-            System.err.println("Server exception: " + e.toString());
+            System.err.println("Middleware server exception: " + e.toString());
             e.printStackTrace();
             throw new RuntimeException("Binding failure: Terminating program ...");
         }
@@ -69,10 +73,9 @@ class MiddlewareServer implements ResourceManager {
             // get the proxy and the remote reference by rmiregistry lookup
             ResourceManager rm = (ResourceManager) registry.lookup(key);
             if(rm!=null) {
-                System.out.println("Successful");
-                System.out.println("Connected to RM");
+                System.out.println("Connected to RM: " + key);
             } else {
-                System.out.println("Unsuccessful");
+                System.err.println("Could not connect to RM: " + key);
             }
             return rm;
         } catch (Exception e) {
@@ -84,101 +87,114 @@ class MiddlewareServer implements ResourceManager {
 
     @Override
     public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice) throws RemoteException {
-        return false;
+        return m_flightRM.addFlight(id, flightNum, flightSeats, flightPrice);
     }
 
     @Override
     public boolean addCars(int id, String location, int numCars, int price) throws RemoteException {
-        return false;
+        return m_carRM.addCars(id, location, numCars, price);
     }
 
     @Override
     public boolean addRooms(int id, String location, int numRooms, int price) throws RemoteException {
-        return false;
+        return m_roomRM.addRooms(id, location, numRooms, price);
     }
 
     @Override
     public int newCustomer(int id) throws RemoteException {
-        return 0;
+        int cid = m_carRM.newCustomer(id);
+        m_roomRM.newCustomer(id, cid);
+        m_flightRM.newCustomer(id, cid);
+        return cid;
     }
 
     @Override
     public boolean newCustomer(int id, int cid) throws RemoteException {
-        return false;
+        return m_flightRM.newCustomer(id, cid) &&
+                m_carRM.newCustomer(id, cid) &&
+                m_roomRM.newCustomer(id, cid);
     }
 
     @Override
     public boolean deleteFlight(int id, int flightNum) throws RemoteException {
-        return false;
+        return m_flightRM.deleteFlight(id, flightNum);
     }
 
     @Override
     public boolean deleteCars(int id, String location) throws RemoteException {
-        return false;
+        return m_carRM.deleteCars(id, location);
     }
 
     @Override
     public boolean deleteRooms(int id, String location) throws RemoteException {
-        return false;
+        return m_roomRM.deleteRooms(id, location);
     }
 
     @Override
     public boolean deleteCustomer(int id, int customer) throws RemoteException {
-        return false;
+        return m_roomRM.deleteCustomer(id, customer) &&
+                m_carRM.deleteCustomer(id, customer) &&
+                m_flightRM.deleteCustomer(id, customer);
     }
 
     @Override
     public int queryFlight(int id, int flightNumber) throws RemoteException {
-        return 0;
+        return m_flightRM.queryFlight(id, flightNumber);
     }
 
     @Override
     public int queryCars(int id, String location) throws RemoteException {
-        return 0;
+        return m_carRM.queryCars(id, location);
     }
 
     @Override
     public int queryRooms(int id, String location) throws RemoteException {
-        return 0;
+        return m_roomRM.queryRooms(id, location);
     }
 
     @Override
     public String queryCustomerInfo(int id, int customer) throws RemoteException {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nCar info:\n").append(m_carRM.queryCustomerInfo(id, customer))
+                .append("\nFlight info:\n").append(m_flightRM.queryCustomerInfo(id, customer))
+                .append("\nRoom info:\n").append(m_roomRM.queryCustomerInfo(id, customer));
+        return sb.toString();
     }
 
     @Override
     public int queryFlightPrice(int id, int flightNumber) throws RemoteException {
-        return 0;
+        return m_flightRM.queryFlightPrice(id, flightNumber);
     }
 
     @Override
     public int queryCarsPrice(int id, String location) throws RemoteException {
-        return 0;
+        return m_carRM.queryCarsPrice(id, location);
     }
 
     @Override
     public int queryRoomsPrice(int id, String location) throws RemoteException {
-        return 0;
+        return m_roomRM.queryRoomsPrice(id, location);
     }
 
     @Override
     public boolean reserveFlight(int id, int customer, int flightNumber) throws RemoteException {
-        return false;
+        return m_flightRM.reserveFlight(id, customer, flightNumber);
     }
 
     @Override
     public boolean reserveCar(int id, int customer, String location) throws RemoteException {
-        return false;
+        return m_carRM.reserveCar(id, customer, location);
     }
 
     @Override
     public boolean reserveRoom(int id, int customer, String locationd) throws RemoteException {
-        return false;
+        return m_roomRM.reserveRoom(id, customer, locationd);
     }
 
     @Override
     public boolean itinerary(int id, int customer, Vector flightNumbers, String location, boolean Car, boolean Room) throws RemoteException {
-        return false;
+        return m_flightRM.itinerary(id, customer, flightNumbers, location, Car, Room) &&
+                m_carRM.itinerary(id, customer, flightNumbers, location, Car, Room) &&
+                m_roomRM.itinerary(id, customer, flightNumbers, location, Car, Room);
     }
 }
