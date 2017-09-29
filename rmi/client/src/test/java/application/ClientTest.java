19f2431b01bd1dfd9d8ef16f3423560130f87d1b
Amir Bawab <amirbawab@gmail.com>
2017-09-28 22:57:45 -0400

    Added and fixed test

--- rmi/client/src/test/java/application/ClientTest.java
+++ rmi/client/src/test/java/application/ClientTest.java
@@ -3,6 +3,7 @@ import inter.ResourceManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.rmi.RemoteException;
@@ -25,6 +26,10 @@ public class ClientTest {
     private final int LOW_TEST = 10;
     private final int MED_TEST = 100;
     private final int HIGH_TEST = 1000;
+
+    // Customer info
+    int id = 1;
+    int cid = 1;
 
     @BeforeClass
     public static void connectToServer() {
@@ -49,11 +54,14 @@ public class ClientTest {
     }
 
     @Test
-    public void flights_test() throws RemoteException, InterruptedException {
+    public void flights_test_1() throws RemoteException, InterruptedException {
 
         // Thread number
         int totalThreads = 5;
         int testLevel = LOW_TEST;
+
+        // Delete customer
+        rm.deleteCustomer(id, cid);
 
         // Remove flights if any
         for(int i=0; i < testLevel * totalThreads; i++) {
@@ -64,7 +72,7 @@ public class ClientTest {
         List<Thread> threadList = new ArrayList<>();
 
         // Start threads
-        for(int t=0; t <= totalThreads; t++) {
+        for(int t=1; t <= totalThreads; t++) {
             // Create thread
             int finalT = t;
             Thread thread = new Thread(new Runnable() {
@@ -91,8 +99,6 @@ public class ClientTest {
             thread.join();
         }
 
-        // TODO Reserve flights
-
         // Query flights
         for(int i=0; i < testLevel * totalThreads; i++) {
             if(rm.queryFlight(i, i) != i || rm.queryFlightPrice(i, i) != i) {
@@ -102,12 +108,81 @@ public class ClientTest {
     }
 
     @Test
-    public void cars_test() throws RemoteException, InterruptedException {
+    public void flights_test_2() throws RemoteException, InterruptedException {
+
+        // Thread number
+        int totalThreads = 2;
+        int testLevel = LOW_TEST;
+
+        // Delete customer
+        rm.deleteCustomer(id, cid);
+
+        // Remove flights if any
+        for(int i=0; i < testLevel * totalThreads; i++) {
+            rm.deleteFlight(i, i);
+        }
+
+        // Add flights
+        for(int i = 0; i < testLevel * totalThreads; i++) {
+            if(!rm.addFlight(i, i, i, i)) {
+                fail(String.format("Failed to add flight id: %d, number: %d", i, i));
+            }
+        }
+
+        // Create customer
+        rm.newCustomer(id, cid);
+
+        // Prepare thread list
+        List<Thread> threadList = new ArrayList<>();
+
+        // Start threads
+        for(int t=1; t <= totalThreads; t++) {
+            int finalT = t;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i=testLevel*(finalT -1); i < testLevel*finalT; i++) {
+                        try {
+
+                            // Reserve i seats
+                            for(int k=0; k < i; k++) {
+                                if(!rm.reserveFlight(i, cid, i)) {
+                                    fail(String.format("Could not reserve seats in flight %d", i));
+                                }
+                            }
+                        } catch (RemoteException e) {
+                            fail("Failed to reserve flight caused by remote exception");
+                        }
+                    }
+                }
+            });
+            thread.start();
+            threadList.add(thread);
+        }
+
+         // Wait until all threads terminate
+        for(Thread thread : threadList) {
+            thread.join();
+        }
+
+        // Query flights
+        for(int i=0; i < testLevel * totalThreads; i++) {
+            if(rm.queryFlight(i, i) != 0) {
+                fail(String.format("Flight id: %d, number: %d was not fully reserved!", i, i));
+            }
+        }
+    }
+
+    @Test
+    public void cars_test_1() throws RemoteException, InterruptedException {
 
         // Thread number
         int totalThreads = 5;
         int testLevel = LOW_TEST;
         String testLocation = "test-location-";
+
+        // Delete customer
+        rm.deleteCustomer(id, cid);
 
         // Remove cars if any
         for(int i=0; i < testLevel * totalThreads; i++) {
@@ -118,7 +193,7 @@ public class ClientTest {
         List<Thread> threadList = new ArrayList<>();
 
         // Start threads
-        for(int t=0; t <= totalThreads; t++) {
+        for(int t=1; t <= totalThreads; t++) {
             // Create thread
             int finalT = t;
             Thread thread = new Thread(new Runnable() {
@@ -145,8 +220,6 @@ public class ClientTest {
             thread.join();
         }
 
-        // TODO Reserve cars
-
         // Query cars
         for(int i=0; i < testLevel * totalThreads; i++) {
             if(rm.queryCars(i, testLocation+i) != i || rm.queryCarsPrice(i, testLocation+i) != i) {
@@ -154,5 +227,4 @@ public class ClientTest {
             }
         }
     }
-
 }
