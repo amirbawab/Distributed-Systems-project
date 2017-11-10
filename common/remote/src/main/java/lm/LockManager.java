55a209721de9a534ef736f8a53b6e21a7938f2c0
Amir Bawab <amirbawab@gmail.com>
2017-11-10 13:40:48 -0500

    Promoted read lock to write lock

--- common/remote/src/main/java/lm/LockManager.java
+++ common/remote/src/main/java/lm/LockManager.java
@@ -203,6 +203,12 @@ public class LockManager
                     // (2) transaction already had a WRITE lock
                     // Seeing the comments at the top of this function might be helpful
                     // *** ADD CODE HERE *** to take care of both these cases
+                    if(dataObj2.getLockType() == DataObj.WRITE) {
+                        throw new RedundantLockRequestException(dataObj.getXId(), "Redundant WRITE lock request");
+                    } else {
+                        dataObj2.setLockType(DataObj.WRITE);
+                        throw new RedundantLockRequestException(dataObj.getXId(), "Lock of type READ promoted to WRITE");
+                    }
                 }
             } 
             else {
